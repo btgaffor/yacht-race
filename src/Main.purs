228d1fc16e8d8960f3f56cef1e3e3283bc27aacf
Brandon Gafford <brandon.gafford@gmail.com>
2020-05-31 19:21:27 -0500

    whole lota stuff

--- src/Main.purs
+++ src/Main.purs
@@ -1,34 +1,309 @@
 module Main where
 
 import Prelude
-
-import Data.Int (toNumber)
+import Control.Monad.Cont (ContT)
+import Control.Monad.Reader (ReaderT, ask, lift, runReaderT)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.Traversable (traverse_)
 import Data.Tuple.Nested ((/\))
-import Ecs (Entity(..), cmap, get, newEntity)
-import EcsCanvas (GameSetup, RenderFrame, StepFrame, runGameEngine)
+import Ecs (Entity(..), Not(..), SystemT, cfold, cmap, cmapM_, destroy, get, modifyGlobal, newEntity, set)
+import EcsCanvas (GameSetup, RenderFrame, StepFrame, StepFrameKeys, runGameEngine)
 import Effect (Effect)
-import Graphics.Canvas (Context2D, fillRect)
-import Model (Position(..), Velocity(..), World, initWorld)
+import Effect.Random (randomInt)
+import Graphics.Canvas
+  ( Context2D
+  , arc
+  , bezierCurveTo
+  , closePath
+  , fillPath
+  , fillText
+  , lineTo
+  , moveTo
+  , rect
+  , setFillStyle
+  , setFont
+  , withContext
+  )
+import Model
+  ( Alien(..)
+  , Bomb(..)
+  , Collision(..)
+  , GameState(..)
+  , GameStateValue(..)
+  , Missile(..)
+  , MissileTimer(..)
+  , Player(..)
+  , Position(..)
+  , Score(..)
+  , Velocity(..)
+  , World
+  , alienComponents
+  , initWorld
+  , missileComponents
+  )
 
 gameSetup :: GameSetup World
 gameSetup = do
-  _ <- newEntity $ Position 20 /\ Velocity 10
-  pure unit
+  void $ newEntity $ Player
+    /\ Position { x: 375.0, y: 520.0 }
+    /\ Velocity { vx: 5.0, vy: 0.0 }
+    /\ Collision { width: 50.0, height: 50.0 }
+    /\ MissileTimer 0
+  traverse_
+    ( \y ->
+        traverse_
+          ( \x -> do
+              t <- lift <<< lift $ randomInt 50 150
+              newEntity
+                $ Alien
+                /\ Position { x, y }
+                /\ Velocity { vx: 2.0, vy: 0.0 }
+                /\ Collision { width: 50.0, height: 50.0 }
+                /\ MissileTimer t
+          )
+          [ 20.0, 100.0, 180.0, 260.0, 340.0, 420.0, 500.0 ]
+    )
+    [ 20.0, 100.0 ]
 
-gameFrame :: StepFrame World
+---------------
+-- StepFrame --
+---------------
+gameFrame :: StepFrameKeys World
 gameFrame keys = do
-  when keys.arrowLeft do cmap $ \(Position p /\ Velocity v) -> Position (p - v)
-  when keys.arrowRight do cmap $ \(Position p /\ Velocity v) -> Position (p + v)
+  GameState state <- get (Entity 0)
+  when (state == Running) do
+    decrementMissileTimers
+    movePlayer keys
+    playerShootMissile keys
+    aliensDropBombs
+    bounceAliens
+    moveNotPlayers
+    clampPlayer
+    checkMissileCollisions
+    checkForVictory
+    penalizeMissedMissiles
+    clearOffScreen
 
+decrementMissileTimers :: SystemT World (ContT Unit Effect) Unit
+decrementMissileTimers = cmap $ \(MissileTimer t) -> MissileTimer (t - 1)
+
+movePlayer :: StepFrameKeys World
+movePlayer keys = do
+  when keys.arrowLeft do
+    cmap $ \(Player /\ Position { x, y } /\ Velocity { vx, vy }) -> Position { x: x - vx, y }
+  when keys.arrowRight do
+    cmap $ \(Player /\ Position { x, y } /\ Velocity { vx, vy }) -> Position { x: x + vx, y }
+
+playerShootMissile :: StepFrameKeys World
+playerShootMissile keys = do
+  when keys.space do
+    cmapM_ \(Player /\ Position { x, y } /\ MissileTimer t) -> do
+      when (t <= 0) do
+        void $ newEntity
+          $ Missile
+          /\ Position { x: x + 20.0, y }
+          /\ Velocity { vx: 0.0, vy: -10.0 }
+          /\ Collision { width: 10.0, height: 15.0 }
+        cmap $ \(Player /\ MissileTimer _) -> MissileTimer 15
+
+aliensDropBombs :: StepFrame World
+aliensDropBombs = do
+  cmapM_ \(Alien /\ (e :: Entity) /\ Position { x, y } /\ MissileTimer t) -> do
+    when (t <= 0) do
+      void $ newEntity
+        $ Bomb
+        /\ Position { x: x + 20.0, y: y + 30.0 }
+        /\ Velocity { vx: 0.0, vy: 10.0 }
+        /\ Collision { width: 10.0, height: 10.0 }
+      newTimer <- lift <<< lift $ randomInt 50 150
+      set e (MissileTimer newTimer)
+
+moveNotPlayers :: StepFrame World
+moveNotPlayers = do
+  cmap $ \(Position { x, y } /\ Velocity { vx, vy } /\ (Not :: Not Player)) -> Position { x: x + vx, y: y + vy }
+
+bounceAliens :: StepFrame World
+bounceAliens = do
+  shouldBounce <-
+    cfold
+      ( \accumulator (Alien /\ Position { x, y } /\ Collision { width, height }) ->
+          accumulator || x <= 0.0 || x + width >= 800.0
+      )
+      false
+  when shouldBounce do cmap $ \(Alien /\ Velocity v) -> Velocity (v { vx = -v.vx })
+
+clampPlayer :: StepFrame World
+clampPlayer = cmap $ \(Player /\ Position { x, y } /\ Collision { width }) -> Position { x: x # min (800.0 - width) # max 0.0, y }
+
+checkMissileCollisions :: StepFrame World
+checkMissileCollisions = do
+  cmapM_
+    $ \(Alien /\ (eAlien :: Entity) /\ (alienP :: Position) /\ (alienC :: Collision)) -> do
+        cmapM_
+          $ \(Missile /\ (eMissile :: Entity) /\ (missileP :: Position) /\ (missileC :: Collision)) -> do
+              when (overlaps alienP alienC missileP missileC) do
+                destroy eAlien alienComponents
+                destroy eMissile missileComponents
+                modifyGlobal $ \(Score score) -> Score (score + 50)
+
+checkForVictory :: StepFrame World
+checkForVictory = do
+  numAliens <- cfold (\accumulator Alien -> accumulator + 1) 0
+  modifyGlobal
+    $ \(GameState state) ->
+        if numAliens == 0 && state == Running then
+          GameState Won
+        else
+          GameState state
+
+overlaps :: Position -> Collision -> Position -> Collision -> Boolean
+overlaps (Position p1) (Collision d1) (Position p2) (Collision d2) =
+  let
+    xOverlaps = p1.x + d1.width >= p2.x && p1.x <= p2.x + d2.width
+
+    yOverlaps = p1.y + d1.height >= p2.y && p1.y <= p2.y + d2.height
+  in
+    xOverlaps && yOverlaps
+
+penalizeMissedMissiles :: StepFrame World
+penalizeMissedMissiles =
+  cmap
+    $ \all@(Missile /\ Position { x, y } /\ Collision { width, height } /\ Velocity _ /\ Score score) ->
+        if (x < 0.0) || (x > 800.0 - width) || (y < 0.0) || (y > 600.0 - width) then
+          Right (Score (score - 10))
+        else
+          Left unit
+
+clearOffScreen :: StepFrame World
+clearOffScreen =
+  cmap
+    $ \all@(Position { x, y } /\ Collision { width, height } /\ Velocity _) ->
+        if (x < 0.0) || (x > 800.0 - width) || (y < 0.0) || (y > 600.0 - width) then
+          Nothing
+        else
+          Just all
+
+-----------------
+-- RenderFrame --
+-----------------
 renderFrame :: RenderFrame World
 renderFrame = do
-  Position p <- get (Entity 0)
-  pure $ \context ->
-    renderPlayer p context
+  -- TODO
+  GameState state <- get (Entity 0)
+  renderMissiles <- cfold (\acc (Missile /\ (p :: Position)) -> acc <> renderMissile p) (pure unit)
+  renderBombs <- cfold (\acc (Bomb /\ (p :: Position)) -> acc <> renderBomb p) (pure unit)
+  renderAliens <- cfold (\acc (Alien /\ (p :: Position)) -> acc <> renderAlien p) (pure unit)
+  renderPlayers <- cfold (\acc (Player /\ (p :: Position)) -> acc <> renderPlayer p) (pure unit)
+  Score score <- get (Entity 0)
+  let
+    render =
+      if (state == Running) then
+        renderMissiles <> renderBombs <> renderAliens <> renderPlayers <> renderScore score
+      else
+        renderVictory score
+  pure $ \context -> runReaderT render context
 
-renderPlayer :: Int -> Context2D -> Effect Unit
-renderPlayer position context =
-  fillRect context { x: toNumber position, y: 5.0, width: 20.0, height: 20.0 }
+renderMissile :: Position -> ReaderT Context2D Effect Unit
+renderMissile (Position { x, y }) = do
+  context <- ask
+  lift
+    $ withContext context do
+        setFillStyle context "gray"
+        fillPath context $ arc context { x: x + 5.0, y: y + 5.0, radius: 5.0, start: 0.0, end: 365.0 }
+        fillPath context $ rect context { x, y: y + 5.0, width: 10.0, height: 10.0 }
+        setFillStyle context "red"
+        fillPath context do
+          moveTo context (x + 8.0) (y + 15.0)
+          lineTo context (x + 5.0) (y + 21.0)
+          lineTo context (x + 2.0) (y + 15.0)
+          closePath context
+  pure unit
 
+renderBomb :: Position -> ReaderT Context2D Effect Unit
+renderBomb (Position { x, y }) = do
+  context <- ask
+  lift
+    $ withContext context do
+        setFillStyle context "gray"
+        fillPath context $ arc context { x: x + 5.0, y: y + 5.0, radius: 5.0, start: 0.0, end: 365.0 }
+  pure unit
+
+renderAlien :: Position -> ReaderT Context2D Effect Unit
+renderAlien (Position { x, y }) = do
+  context <- ask
+  lift
+    $ withContext context do
+        setFillStyle context "green"
+        renderEllipse context (x + 25.0) (y + 20.0) 40.0 20.0
+        renderEllipse context (x + 5.0) (y + 25.0) 10.0 30.0
+        renderEllipse context (x + 45.0) (y + 25.0) 10.0 30.0
+        renderEllipse context (x + 25.0) (y + 25.0) 20.0 50.0
+        setFillStyle context "lightgray"
+        renderEllipse context (x + 25.0) (y + 35.0) 5.0 10.0
+  pure unit
+
+renderPlayer :: Position -> ReaderT Context2D Effect Unit
+renderPlayer (Position { x, y }) = do
+  context <- ask
+  lift
+    $ withContext context do
+        setFillStyle context "lightgray"
+        fillPath context do
+          moveTo context (x + 25.0) y
+          lineTo context (x + 50.0) (y + 50.0)
+          lineTo context x (y + 50.0)
+          closePath context
+        setFillStyle context "red"
+        fillPath context do
+          moveTo context (x + 20.0) (y + 50.0)
+          lineTo context (x + 15.0) (y + 60.0)
+          lineTo context (x + 10.0) (y + 50.0)
+          closePath context
+        fillPath context do
+          moveTo context (x + 40.0) (y + 50.0)
+          lineTo context (x + 35.0) (y + 60.0)
+          lineTo context (x + 30.0) (y + 50.0)
+          closePath context
+  pure unit
+
+renderScore :: Int -> ReaderT Context2D Effect Unit
+renderScore score = do
+  context <- ask
+  lift
+    $ withContext context do
+        setFillStyle context "white"
+        fillText context ("Score: " <> show score) 20.0 590.0
+  pure unit
+
+renderVictory :: Int -> ReaderT Context2D Effect Unit
+renderVictory score = do
+  context <- ask
+  lift
+    $ withContext context do
+        setFillStyle context "white"
+        setFont context "64px sans-serif"
+        fillText context ("Victory!") 270.0 280.0
+        fillText context ("Final Score: " <> show score) 150.0 350.0
+  pure unit
+
+renderEllipse :: Context2D -> Number -> Number -> Number -> Number -> Effect Unit
+renderEllipse context x y width height =
+  let
+    widthOver2 = width / 2.0
+
+    widthTwoThirds = width * 2.0 / 3.0
+
+    heightOver2 = height / 2.0
+  in
+    fillPath context do
+      moveTo context x (y - heightOver2)
+      bezierCurveTo context { cp1x: x + widthTwoThirds, cp1y: y - heightOver2, cp2x: x + widthTwoThirds, cp2y: y + heightOver2, x, y: y + heightOver2 }
+      bezierCurveTo context { cp1x: x - widthTwoThirds, cp1y: y + heightOver2, cp2x: x - widthTwoThirds, cp2y: y - heightOver2, x, y: y - heightOver2 }
+      closePath context
+
+----------
+-- Main --
+----------
 main :: Effect Unit
 main = runGameEngine initWorld gameSetup gameFrame renderFrame
--- src/Model.purs
+++ src/Model.purs
@@ -4,28 +4,58 @@ import Prelude
 
 import Control.Monad.State (gets, modify_)
 import Data.Map (Map)
-import Ecs (class GetStore, class SaveStore, Entity, EntityCount, Global, initStore)
+import Data.Tuple (Tuple)
+import Ecs (class GetStore, class SaveStore, Entity, EntityCount, Global(..), Unique, initStore)
 import Effect (Effect)
+import Type.Prelude (Proxy(..))
+
+data Score = Score Int
+
+data GameStateValue = Running | Won
+derive instance eqGameStateValue :: Eq GameStateValue
+
+data GameState = GameState GameStateValue
+
+data Player
+  = Player
+
+data Alien = Alien
+alienComponents = Proxy :: Proxy (Tuple Alien (Tuple Position Collision))
+
+data Missile = Missile
+missileComponents = Proxy :: Proxy (Tuple Missile (Tuple Position Collision))
+
+data Bomb = Bomb
+bombComponents = Proxy :: Proxy (Tuple Bomb (Tuple Position Collision))
 
 data Position
-  = Position Int
+  = Position { x :: Number, y :: Number }
 
 data Velocity
-  = Velocity Int
+  = Velocity { vx :: Number, vy :: Number }
 
-data World
-  = World
-    { entityCounter :: Global EntityCount
-    , positions :: Map Entity Position
-    , velocities :: Map Entity Velocity
-    }
+data Collision
+  = Collision { width :: Number, height :: Number }
 
-unWorld ::
-  World ->
+data MissileTimer = MissileTimer Int
+
+type WorldInner =
   { entityCounter :: Global EntityCount
-  , positions :: Map Entity Position
-  , velocities :: Map Entity Velocity
+  , score :: Global Score
+  , gameState :: Global GameState
+  , player :: Unique Player
+  , alien :: Map Entity Alien
+  , missile :: Map Entity Missile
+  , bomb :: Map Entity Bomb
+  , position :: Map Entity Position
+  , velocity :: Map Entity Velocity
+  , drawable :: Map Entity Collision
+  , missileTimer :: Map Entity MissileTimer
   }
+
+data World = World WorldInner
+
+unWorld :: World -> WorldInner
 unWorld (World world) = world
 
 initWorld :: Effect World
@@ -33,24 +63,113 @@ initWorld = do
   pure
     $ World
         { entityCounter: initStore
-        , positions: initStore
-        , velocities: initStore
+        , score: Global (Score 0)
+        , gameState: Global (GameState Running)
+        , player: initStore
+        , alien: initStore
+        , missile: initStore
+        , bomb: initStore
+        , position: initStore
+        , velocity: initStore
+        , drawable: initStore
+        , missileTimer: initStore
         }
 
+-------------------
+-- EntityCounter --
+-------------------
 instance hasEntityCounter :: GetStore World EntityCount (Global EntityCount) where
   getStore _ = gets (unWorld >>> _.entityCounter)
 
 instance saveStoreEntityCounter :: SaveStore World (Global EntityCount) where
-  saveStore entityCounter = modify_ (unWorld >>> _ { entityCounter = entityCounter } >>> World)
+  saveStore value = modify_ (unWorld >>> _ { entityCounter = value } >>> World)
 
+------------
+-- Player --
+------------
+instance hasPlayer :: GetStore World Player (Unique Player) where
+  getStore _ = gets (unWorld >>> _.player)
+
+instance saveStorePlayer :: SaveStore World (Unique Player) where
+  saveStore value = modify_ (unWorld >>> _ { player = value } >>> World)
+
+-----------
+-- Alien --
+-----------
+instance hasAlien :: GetStore World Alien (Map Entity Alien) where
+  getStore _ = gets (unWorld >>> _.alien)
+
+instance saveStoreAlien :: SaveStore World (Map Entity Alien) where
+  saveStore value = modify_ (unWorld >>> _ { alien = value } >>> World)
+
+-------------
+-- Missile --
+-------------
+instance hasMissile :: GetStore World Missile (Map Entity Missile) where
+  getStore _ = gets (unWorld >>> _.missile)
+
+instance saveStoreMissile :: SaveStore World (Map Entity Missile) where
+  saveStore value = modify_ (unWorld >>> _ { missile = value } >>> World)
+
+----------
+-- Bomb --
+----------
+instance hasBomb :: GetStore World Bomb (Map Entity Bomb) where
+  getStore _ = gets (unWorld >>> _.bomb)
+
+instance saveStoreBomb :: SaveStore World (Map Entity Bomb) where
+  saveStore value = modify_ (unWorld >>> _ { bomb = value } >>> World)
+
+--------------
+-- Position --
+--------------
 instance getStorePosition :: GetStore World Position (Map Entity Position) where
-  getStore _ = gets (unWorld >>> _.positions)
+  getStore _ = gets (unWorld >>> _.position)
 
 instance saveStorePosition :: SaveStore World (Map Entity Position) where
-  saveStore value = modify_ (unWorld >>> _ { positions = value } >>> World)
+  saveStore value = modify_ (unWorld >>> _ { position = value } >>> World)
 
+--------------
+-- Velocity --
+--------------
 instance getStoreVelocity :: GetStore World Velocity (Map Entity Velocity) where
-  getStore _ = gets (unWorld >>> _.velocities)
+  getStore _ = gets (unWorld >>> _.velocity)
 
 instance saveStoreVelocity :: SaveStore World (Map Entity Velocity) where
-  saveStore value = modify_ (unWorld >>> _ { velocities = value } >>> World)
+  saveStore value = modify_ (unWorld >>> _ { velocity = value } >>> World)
+
+---------------
+-- Collision --
+---------------
+instance getStoreCollision :: GetStore World Collision (Map Entity Collision) where
+  getStore _ = gets (unWorld >>> _.drawable)
+
+instance saveStoreCollision :: SaveStore World (Map Entity Collision) where
+  saveStore value = modify_ (unWorld >>> _ { drawable = value } >>> World)
+
+------------------
+-- MissileTimer --
+------------------
+instance getStoreMissileTimer :: GetStore World MissileTimer (Map Entity MissileTimer) where
+  getStore _ = gets (unWorld >>> _.missileTimer)
+
+instance saveStoreMissileTimer :: SaveStore World (Map Entity MissileTimer) where
+  saveStore value = modify_ (unWorld >>> _ { missileTimer = value } >>> World)
+
+-----------
+-- Score --
+-----------
+instance getStoreScore :: GetStore World Score (Global Score) where
+  getStore _ = gets (unWorld >>> _.score)
+
+instance saveStoreScore :: SaveStore World (Global Score) where
+  saveStore value = modify_ (unWorld >>> _ { score = value } >>> World)
+
+---------------
+-- GameState --
+---------------
+instance getStoreGameState :: GetStore World GameState (Global GameState) where
+  getStore _ = gets (unWorld >>> _.gameState)
+
+instance saveStoreGameState :: SaveStore World (Global GameState) where
+  saveStore value = modify_ (unWorld >>> _ { gameState = value } >>> World)
